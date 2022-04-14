7c0691382ac2a66e608fe6a754a65c1a32fd6f0d
Iñaki Calvo <inakinaiz@gmail.com>
2022-04-14 09:28:21 +0200

    CSS styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
 
     const d = new Date();
   return (
-    <footer>&#169; {d.getFullYear()} - Built with patience and React by Iñaki Calvo.</footer>
+    <footer>&#169; {d.getFullYear()} - Built with patience and React JS by Iñaki Calvo.</footer>
   )
 }
 
